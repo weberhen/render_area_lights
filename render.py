d8918d3fb87b02c8b7949307ce8fb8afbae8f532
henrique weber <iqueweber@gmail.com>
2021-03-24 17:10:21 -0300

    cleaner code

--- render.py
+++ render.py
@@ -4,11 +4,7 @@ from scipy.ndimage.interpolation import zoom
 import imageio
 import numpy as np
 
-# obj_positions = [
-#                  [.25,-.21,4],
-#                  [.25,-.18,5],
-#                  [.25,.05,10] ]
-steps = 80
+steps = 8
 obj_positions = [[.25, i/100, j] for i,j in zip(np.linspace(-27,0,steps), np.linspace(1,10,steps))]
 imInput = imageio.imread(os.path.join('class.jpg')).astype('float32') / 255.
 imInputResized = zoom(imInput, (400/imInput.shape[0], 600/imInput.shape[1], 1.0), order=1)
@@ -16,35 +12,27 @@ imInputResized = zoom(imInput, (400/imInput.shape[0], 600/imInput.shape[1], 1.0)
 command = 'mitsuba render_pano.xml'
 os.system(command)
 
-for i, obj_pos in enumerate(obj_positions):
-    command = 'mitsuba -Dobjx='+str(obj_pos[0])+' -Dobjy='+str(obj_pos[1])+' -Dobjz='+str(obj_pos[2])+' render_sphere.xml'
-    print(command)
-    os.system(command)
-    
-    os.system('mtsutil tonemap -m 100 -o render_'+str(i).zfill(3)+'.png render_sphere.exr')
-    
-    command = 'mitsuba -Dobjx='+str(obj_pos[0])+' -Dobjy='+str(obj_pos[1])+' -Dobjz='+str(obj_pos[2])+' render_sphere_envmap.xml'
-    print(command)
-    os.system(command)
-    
-    os.system('mtsutil tonemap -m 100 -o render_envmap_'+str(i).zfill(3)+'.png render_sphere_envmap.exr')
-    
-    imRender = hdrio.imread('render_sphere.exr').astype('float32')
-    
-    alphaRender = imRender[..., 3:4]
-    imRender = (imRender[..., :3]*100)**(1/2.2)
-    imCompose = (1-alphaRender)*imInputResized + alphaRender*imRender
+scenes = ['render_sphere_envmap', 'render_sphere']
+# scenes = ['render_armadillo_envmap', 'render_armadillo']
 
-    imComposeUint8 = (np.clip(imCompose, 0.0, 1.0)*255).astype('uint8')
+for scene in scenes:
+    for i, obj_pos in enumerate(obj_positions):
+        command = 'mitsuba -Dobjx='+str(obj_pos[0])+' -Dobjy='+str(obj_pos[1])+' -Dobjz='+str(obj_pos[2])+' '+scene+'.xml'
+        print(command)
+        os.system(command)
+        
+        os.system('mtsutil tonemap -m 100 -o render_'+str(i).zfill(3)+'.png '+scene+'.exr')
+        
+        imRender = hdrio.imread(scene+'.exr').astype('float32')
+        
+        alphaRender = imRender[..., 3:4]
+        imRender = (imRender[..., :3]*100)**(1/2.2)
+        imCompose = (1-alphaRender)*imInputResized + alphaRender*imRender
 
-    imageio.imsave('composite_'+str(i).zfill(3)+'.png', imComposeUint8)
+        imComposeUint8 = (np.clip(imCompose, 0.0, 1.0)*255).astype('uint8')
+
+        imageio.imsave(scene+'_composite_'+str(i).zfill(3)+'.png', imComposeUint8)
+        
+    os.system('convert -delay 10 '+scene+'_composite_*.png -loop 0 '+scene+'_render.gif')
+    os.system('rm *.png && rm *.exr && rm *.log')
     
-    imRender = hdrio.imread('render_sphere_envmap.exr').astype('float32')
-    
-    alphaRender = imRender[..., 3:4]
-    imRender = (imRender[..., :3]*100)**(1/2.2)
-    imCompose = (1-alphaRender)*imInputResized + alphaRender*imRender
-
-    imComposeUint8 = (np.clip(imCompose, 0.0, 1.0)*255).astype('uint8')
-
-    imageio.imsave('envmap_composite_'+str(i).zfill(3)+'.png', imComposeUint8)
